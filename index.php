92c2d2f232251fbcc3b98dcd522a9e86307f31cd
morhsn <mor.hsn@gmail.com>
2015-11-28 15:15:28 +0200

    Adding under construction index.php file

---
+++ index.php
@@ -0,0 +1,7 @@
+<html>
+<head><title>Welcome to MyBookSharing</title></head>
+<body>
+<img style="display: block; margin-left: auto; margin-right: auto;"
+     src="http://www.laminaresearchcenter.com/images/comingsoon.png"/>
+</body>
+</html>
